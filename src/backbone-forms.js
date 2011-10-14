ad7a81288e2d79a5ec18c3abb83835ea19c17a9c
Marc Tamlyn <marc.tamlyn@gmail.com>
2011-10-14 13:39:41 +0100

    Add radio editor.
    Added a new radio editor based off the select editor. Tests included.

--- src/backbone-forms.js
+++ src/backbone-forms.js
@@ -707,6 +707,59 @@
 
 
     /**
+     * Renders a <ul> with given options represented as <li> objects containing radio buttons
+     *
+     * Requires an 'options' value on the schema.
+     *  Can be an array of options, a function that calls back with the array of options, a string of HTML
+     *  or a Backbone collection. If a collection, the models must implement a toString() method
+     */
+    editors.Radio = editors.Select.extend({
+
+        tagName: 'ul',
+        className: 'bbf-radio',
+
+        getValue: function() {
+            return this.$('input[type=radio]').filter(function () { return this.checked }).val();
+        },
+
+        setValue: function(value) {
+            return this.$('input[type=radio]').filter(function () { return value == $(this).val() }).attr({checked: 'checked'});
+        },
+
+        /**
+         * Create the radio list HTML
+         * @param {Array}   Options as a simple array e.g. ['option1', 'option2']
+         *                      or as an array of objects e.g. [{val: 543, label: 'Title for object 543'}]
+         * @return {String} HTML
+         */
+        _arrayToHtml: function (array) {
+            var html = [];
+            var self = this;
+
+            _.each(array, function(option, index) {
+                var itemHtml = '<li>';
+                if (_.isObject(option)) {
+                    var val = option.val ? option.val : '';
+                    itemHtml += ('<input type="radio" name="'+self.id+'" value="'+val+'" id="'+self.id+'-'+index+'" />')
+                    itemHtml += ('<label for="'+self.id+'-'+index+'">'+option.label+'</label>')
+                }
+                else {
+                    itemHtml += ('<input type="radio" name="'+self.id+'" value="'+option+'" id="'+self.id+'-'+index+'" />')
+                    itemHtml += ('<label for="'+self.id+'-'+index+'">'+option+'</label>')
+                }
+                itemHtml += '</li>';
+                html.push(itemHtml);
+            });
+
+            return html.join('');
+        },
+
+    });
+
+
+
+
+    /**
      * Creates a child form. For editing Javascript objects
      * 
      * Special options:
