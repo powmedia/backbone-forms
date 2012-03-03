3c4dc39db1d5a64f317d1c2234493b4d382ba1e9
Phil Freo <phil@philfreo.com>
2012-03-02 16:53:32 -0800

    Support the 'title' attribute on radio inputs to provide additional info

--- src/backbone-forms.js
+++ src/backbone-forms.js
@@ -1150,7 +1150,7 @@
     /**
      * Create the radio list HTML
      * @param {Array}   Options as a simple array e.g. ['option1', 'option2']
-     *                      or as an array of objects e.g. [{val: 543, label: 'Title for object 543'}]
+     *                      or as an array of objects e.g. [{val: 543, label: 'Label for object 543', title: 'Title hint shown on mouse hover'}]
      * @return {String} HTML
      */
     _arrayToHtml: function (array) {
@@ -1160,8 +1160,9 @@
       _.each(array, function(option, index) {
         var itemHtml = '<li>';
         if (_.isObject(option)) {
-          var val = option.val ? option.val : '';
-          itemHtml += ('<input type="radio" name="'+self.id+'" value="'+val+'" id="'+self.id+'-'+index+'" />')
+          var val = option.val ? option.val : '',
+              title = option.title ? ' title="'+option.title+'"' : '';
+          itemHtml += ('<input type="radio" name="'+self.id+'" value="'+val+'" id="'+self.id+'-'+index+'"'+title+' />')
           itemHtml += ('<label for="'+self.id+'-'+index+'">'+option.label+'</label>')
         }
         else {
