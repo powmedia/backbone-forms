01ed6b50be865fe5310244f86a45a63a64f8c5a7
Phil Freo <phil@philfreo.com>
2012-06-25 13:44:36 -0700

    Give default template field outer elements a classname based on field name so they can be styled

--- src/templates/default.js
+++ src/templates/default.js
@@ -15,7 +15,7 @@
     ',
     
     field: '\
-      <li class="bbf-field">\
+      <li class="bbf-field field-{{key}}">\
         <label for="{{id}}">{{title}}</label>\
         <div class="bbf-editor">{{editor}}</div>\
         <div class="bbf-help">{{help}}</div>\
@@ -23,7 +23,7 @@
     ',
 
     nestedField: '\
-      <li class="bbf-field" title="{{title}}">\
+      <li class="bbf-field bbf-nested-field field-{{key}}" title="{{title}}">\
         <label for="{{id}}">{{title}}</label>\
         <div class="bbf-editor">{{editor}}</div>\
         <div class="bbf-help">{{help}}</div>\
