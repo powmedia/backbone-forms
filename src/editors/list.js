ba936960578eb71cd556a4d1c608654abfa386e0
Richard Mitchell <richard.j.mitchell@gmail.com>
2013-03-06 18:24:02 +0000

    Pass form through to list item editors. Custom editors may require it.

--- src/editors/list.js
+++ src/editors/list.js
@@ -97,7 +97,8 @@
         schema: this.schema,
         value: value,
         Editor: this.Editor,
-        key: this.key
+        key: this.key,
+        form: this.form
       }).render();
       
       var _addItem = function() {
@@ -278,6 +279,7 @@
       this.value = options.value;
       this.Editor = options.Editor || editors.Text;
       this.key = options.key;
+      this.form = options.form;
     },
 
     render: function() {
@@ -287,7 +289,8 @@
         schema: this.schema,
         value: this.value,
         list: this.list,
-        item: this
+        item: this,
+        form: this.form
       }).render();
 
       //Create main element
