c04eafa7722e2ba2da5e4775dbfc10a792340a03
Phil Freo <phil@philfreo.com>
2013-03-08 11:18:40 -0800

    Temporary workaround for .is(':focus') not working in phantomjs; makes some tests pass but still some focus/blur tests failing, not sure why

--- test/_setup.js
+++ test/_setup.js
@@ -2,6 +2,16 @@ var same = deepEqual;
 
 sinon.config.useFakeTimers = false;
 
+// Patch $el.is(':focus') until PhantomJS supports it properly.
+// https://code.google.com/p/phantomjs/issues/detail?id=427
+var _jQuery_is = jQuery.fn.is;
+jQuery.fn.is = function(s) {
+    if (s === ':focus') {
+        return this.get(0) === document.activeElement;
+    }
+    return _jQuery_is.apply(this, arguments);
+};
+
 var Post = Backbone.Model.extend({
     defaults: {
         title: 'Danger Zone!',
