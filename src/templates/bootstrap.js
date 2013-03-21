d568766737abf3ec2fab3111fa765cf46a113b5b
Charles Davison <charlie@powmedia.co.uk>
2013-03-21 19:11:32 +0000

    Just override default templates for bootstrap instead of extending form

--- src/templates/bootstrap.js
+++ src/templates/bootstrap.js
@@ -4,57 +4,41 @@
  * Include this file after backbone-forms.js and use Backbone.BootstrapForm instead
  * of Backbone.Form
  */
-Backbone.BootstrapForm = Backbone.Form.extend({
-
-  template: _.template($.trim('\
-    <form class="form-horizontal" data-fieldsets></form>\
-  '))
-
-});
+Backbone.Form.prototype.template = _.template($.trim('\
+  <form class="form-horizontal" data-fieldsets></form>\
+'));
 
 
-Backbone.BootstrapForm.Fieldset = Backbone.Form.Fieldset.extend({
-
-  template: _.template($.trim('\
-    <fieldset data-fields>\
-      <% if (legend) { %>\
-        <legend><%= legend %></legend>\
-      <% } %>\
-    </fieldset>\
-  '))
-
-});
+Backbone.Form.Fieldset.prototype.template = _.template($.trim('\
+  <fieldset data-fields>\
+    <% if (legend) { %>\
+      <legend><%= legend %></legend>\
+    <% } %>\
+  </fieldset>\
+'));
 
 
-Backbone.BootstrapForm.Field = Backbone.Form.Field.extend({
-
-  template: _.template($.trim('\
-    <div class="control-group field-<%= key %>">\
-      <label class="control-label" for="<%= editorId %>"><%= title %></label>\
-      <div class="controls">\
-        <span data-editor></span>\
-        <div class="help-inline" data-error></div>\
-        <div class="help-block"><%= help %></div>\
-      </div>\
-    </div>\
-  '))
-
-});
-
-
-Backbone.BootstrapForm.NestedField = Backbone.Form.Field.extend({
-
-  template: _.template($.trim('\
-    <div class="field-<%= key %>">\
-      <div title="<%= title %>" class="input-xlarge">\
-        <span data-editor></span>\
-        <div class="help-inline" data-error></div>\
-      </div>\
+Backbone.Form.Field.prototype.template = _.template($.trim('\
+  <div class="control-group field-<%= key %>">\
+    <label class="control-label" for="<%= editorId %>"><%= title %></label>\
+    <div class="controls">\
+      <span data-editor></span>\
+      <div class="help-inline" data-error></div>\
       <div class="help-block"><%= help %></div>\
     </div>\
-  '))
+  </div>\
+'));
 
-});
+
+Backbone.Form.NestedField.prototype.template = _.template($.trim('\
+  <div class="field-<%= key %>">\
+    <div title="<%= title %>" class="input-xlarge">\
+      <span data-editor></span>\
+      <div class="help-inline" data-error></div>\
+    </div>\
+    <div class="help-block"><%= help %></div>\
+  </div>\
+'));
 
 
 Backbone.Form.editors.List.prototype.template = _.template($.trim('\
@@ -63,6 +47,7 @@ Backbone.Form.editors.List.prototype.template = _.template($.trim('\
     <button class="btn bbf-add" data-action="add">Add</button>\
   </div>\
 '));
+
 
 Backbone.Form.editors.List.Item.prototype.template = _.template($.trim('\
   <li class="clearfix">\
