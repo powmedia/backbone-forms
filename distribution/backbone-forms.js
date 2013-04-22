92b1463e90953d9cd11a7eee8fc70a56bbe6794e
Ollie Glass <ollie.glass@peerindex.com>
2013-04-22 18:12:54 +0100

    Bootstrap-friendly checkbox inputs

--- distribution/backbone-forms.js
+++ distribution/backbone-forms.js
@@ -1820,12 +1820,10 @@ Form.editors.Checkboxes = Form.editors.Select.extend({
       var itemHtml = '<li>';
       if (_.isObject(option)) {
         var val = (option.val || option.val === 0) ? option.val : '';
-        itemHtml += ('<input type="checkbox" name="'+self.id+'" value="'+val+'" id="'+self.id+'-'+index+'" />');
-        itemHtml += ('<label for="'+self.id+'-'+index+'">'+option.label+'</label>');
+        itemHtml += ('<label class="checkbox" for="'+self.id+'-'+index+'">' + '<input type="checkbox" name="'+self.id+'" value="'+val+'" id="'+self.id+'-'+index+'" />' + option.label+'</label>');
       }
       else {
-        itemHtml += ('<input type="checkbox" name="'+self.id+'" value="'+option+'" id="'+self.id+'-'+index+'" />');
-        itemHtml += ('<label for="'+self.id+'-'+index+'">'+option+'</label>');
+        itemHtml += ('<label class="checkbox" for="'+self.id+'-'+index+'">' + '<input type="checkbox" name="'+self.id+'" value="'+option+'" id="'+self.id+'-'+index+'" />' + option+'</label>');
       }
       itemHtml += '</li>';
       html.push(itemHtml);
