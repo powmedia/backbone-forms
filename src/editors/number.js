d1c314ddec36f073d4410f2af115d099c0cb502e
Jarek Piórkowski <jarek@piorkowski.ca>
2013-07-21 00:26:56 -0700

    Don't overwrite step parameter in number editor

--- src/editors/number.js
+++ src/editors/number.js
@@ -14,8 +14,15 @@ Form.editors.Number = Form.editors.Text.extend({
   initialize: function(options) {
     Form.editors.Text.prototype.initialize.call(this, options);
 
+    var schema = this.schema;
+
     this.$el.attr('type', 'number');
-    this.$el.attr('step', 'any');
+
+    if (!schema || !schema.editorAttrs || !schema.editorAttrs.step) {
+      // provide a default for `step` attr,
+      // but don't overwrite if already specified
+      this.$el.attr('step', 'any');
+    }
   },
 
   /**
