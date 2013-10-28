406e1452d804e8d6c0c912b80a8c248a112230d6
Robbie Mackay <rm@robbiemackay.com>
2013-10-28 13:08:09 +1300

    Move initialization of nestedForm to initialize to ensure its always present
    Otherwise we get errors when validating fields that weren't rendered

--- src/editors/object.js
+++ src/editors/object.js
@@ -22,9 +22,7 @@ Form.editors.Object = Form.editors.Base.extend({
     //Check required options
     if (!this.form) throw new Error('Missing required option "form"');
     if (!this.schema.subSchema) throw new Error("Missing required 'schema.subSchema' option for Object editor");
-  },
 
-  render: function() {
     //Get the constructor for creating the nested form; i.e. the same constructor as used by the parent form
     var NestedForm = this.form.NestedForm;
 
@@ -35,6 +33,9 @@ Form.editors.Object = Form.editors.Base.extend({
       idPrefix: this.id + '_',
       Field: NestedForm.NestedField
     });
+  },
+
+  render: function() {
 
     this._observeFormEvents();
 
