14753a30229b138fd92bc9601bfa43f136214a72
Geert-Jan Brits <gbrits@gmail.com>
2014-02-15 01:38:52 +0100

    Update backbone-forms.js

--- distribution.amd/backbone-forms.js
+++ distribution.amd/backbone-forms.js
@@ -675,14 +675,18 @@ Form.Fieldset = Backbone.View.extend({
       var $container = $(el),
           selection = $container.attr('data-fields');
 
-      if (_.isUndefined(selection)) return;
+      if (_.isUndefined(selection)) {
+      	return;
+      }
       
       //Work out which fields to include (_compact used to trim empty string selection)
       var keys = _.compact((selection == '*') ? self.selectedFields || _.keys(fields) : selection.split(','));
       
       //include all keys if not specified
-      if (!keys.length)
-          keys = _.keys(fields);
+      if (!keys.length){
+      	keys = _.keys(fields);
+      }
+          
 
       //Add them
       _.each(keys, function(key) {
