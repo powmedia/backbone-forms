be48ab79dc12ff1945f05ca7c4df5a8b83ed5441
Yannick Fonjallaz <yannick.fonjallaz@nck.aphp.fr>
2014-06-27 10:55:36 +0200

    Define fieldsets on model

--- src/form.js
+++ src/form.js
@@ -70,7 +70,7 @@ var Form = Backbone.View.extend({
     }, this);
 
     //Create fieldsets
-    var fieldsetSchema = options.fieldsets || _.result(this, 'fieldsets') || [selectedFields],
+    var fieldsetSchema = options.fieldsets || _.result(this, 'fieldsets') || _.result(this.model, 'fieldsets') || [selectedFields],
         fieldsets = this.fieldsets = [];
 
     _.each(fieldsetSchema, function(itemSchema) {
