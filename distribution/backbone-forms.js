b3a4e9243d076535132c10b7a108957b2d063e28
Yannick Fonjallaz <yannick.fonjallaz@nck.aphp.fr>
2016-02-01 11:40:59 +0100

    Number validator accepts '.5'

--- distribution/backbone-forms.js
+++ distribution/backbone-forms.js
@@ -497,7 +497,7 @@ var Form = Backbone.View.extend({
 
 });
 
-  
+
 //==================================================================================================
 //VALIDATORS
 //==================================================================================================
@@ -515,42 +515,42 @@ Form.validators = (function() {
     url: 'Invalid URL',
     match: _.template('Must match field "<%= field %>"', null, Form.templateSettings)
   };
-  
+
   validators.required = function(options) {
     options = _.extend({
       type: 'required',
       message: this.errMessages.required
     }, options);
-     
+
     return function required(value) {
       options.value = value;
-      
+
       var err = {
         type: options.type,
         message: _.isFunction(options.message) ? options.message(options) : options.message
       };
-      
+
       if (value === null || value === undefined || value === false || value === '') return err;
     };
   };
-  
+
   validators.regexp = function(options) {
     if (!options.regexp) throw new Error('Missing required "regexp" option for "regexp" validator');
-  
+
     options = _.extend({
       type: 'regexp',
       match: true,
       message: this.errMessages.regexp
     }, options);
-    
+
     return function regexp(value) {
       options.value = value;
-      
+
       var err = {
         type: options.type,
         message: _.isFunction(options.message) ? options.message(options) : options.message
       };
-      
+
       //Don't check empty values (add a 'required' validator for this)
       if (value === null || value === undefined || value === '') return;
 
@@ -565,9 +565,9 @@ Form.validators = (function() {
     options = _.extend({
       type: 'number',
       message: this.errMessages.number,
-      regexp: /^-?[0-9]+(\.[0-9]+)?$/
+      regexp: /^[-+]?([0-9]*.[0-9]+|[0-9]+)$/
     }, options);
-    
+
     return validators.regexp(options);
   };
 
@@ -599,46 +599,46 @@ Form.validators = (function() {
       if (number < options.min || number > options.max) return err;
     }
   }
-  
+
   validators.email = function(options) {
     options = _.extend({
       type: 'email',
       message: this.errMessages.email,
       regexp: /^[\w\-]{1,}([\w\-\+.]{1,1}[\w\-]{1,}){0,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/
     }, options);
-    
+
     return validators.regexp(options);
   };
-  
+
   validators.url = function(options) {
     options = _.extend({
       type: 'url',
       message: this.errMessages.url,
       regexp: /^(http|https):\/\/(([A-Z0-9][A-Z0-9_\-]*)(\.[A-Z0-9][A-Z0-9_\-]*)+)(:(\d+))?\/?/i
     }, options);
-    
+
     return validators.regexp(options);
   };
-  
+
   validators.match = function(options) {
     if (!options.field) throw new Error('Missing required "field" options for "match" validator');
-    
+
     options = _.extend({
       type: 'match',
       message: this.errMessages.match
     }, options);
-    
+
     return function match(value, attrs) {
       options.value = value;
-      
+
       var err = {
         type: options.type,
         message: _.isFunction(options.message) ? options.message(options) : options.message
       };
-      
+
       //Don't check empty values (add a 'required' validator for this)
       if (value === null || value === undefined || value === '') return;
-      
+
       if (value !== attrs[options.field]) return err;
     };
   };
--- test/validators.js
+++ test/validators.js
@@ -5,26 +5,26 @@
 ;(function() {
 
   module('general')
-  
+
   test('can change default error messages with mustache tags', function() {
     var originalMessage = Form.validators.errMessages.email;
-    
+
     Form.validators.errMessages.email = _.template('<%= value %> is an invalid email address. <%= customTag %>.', null, Form.templateSettings);
-    
+
     var email = Form.validators.email({ customTag: 'Cool beans' })
     equal(email('foo').message, 'foo is an invalid email address. Cool beans.')
-    
+
     //Restore original message
     Form.validators.errMessages.email = originalMessage;
   })
-  
+
 })();
 
 
 ;(function() {
 
   module('required')
-  
+
   var required = Form.validators.required()
 
   test('error if field is null or undefined or false', function() {
@@ -32,16 +32,16 @@
     ok(required())
     ok(required(false))
   })
-  
+
   test('error if field is empty string', function() {
     ok(required(''))
     equal(required('test', undefined))
   })
-  
+
   test('ok if field is number 0', function() {
     equal(required(0), undefined)
   })
-  
+
   test('ok if field is boolean true', function() {
     equal(required(true), undefined)
   })
@@ -53,7 +53,7 @@
 
   module('regexp')
 
-  //Main  
+  //Main
   var fn = Form.validators.regexp({
     regexp: /foo/
   });
@@ -63,12 +63,12 @@
     equal(fn(null), undefined)
     equal(fn(undefined), undefined)
   })
-  
+
   test('fails invalid strings', function() {
     equal(fn('gsurkbfsr').type, 'regexp')
     equal(fn('guerbayf').message, 'Invalid')
   })
-  
+
   test('passes valid strings', function() {
     equal(fn('foo'), undefined)
     equal(fn('_foo_'), undefined)
@@ -141,23 +141,22 @@
 
 ;(function() {
   module('number')
-  
+
   var fn = Form.validators.number()
-  
+
   test('passes empty values', function() {
     equal(fn(''), undefined)
     equal(fn(null), undefined)
     equal(fn(undefined), undefined)
   })
-  
+
   test('fails non-number values', function() {
     ok(fn('foo'))
     ok(fn('123a'))
     ok(fn('-.'))
-    ok(fn('.5'))
     ok(fn('5.'))
   })
-  
+
   test('accepts numbers', function() {
     equal(fn('123'), undefined)
     equal(fn(456), undefined)
@@ -165,56 +164,57 @@
     equal(fn('123.5'), undefined)
     equal(fn('-123.5'), undefined)
     equal(fn(-123.5), undefined)
+    equal(fn('.5'), undefined)
     equal(fn(0.5), undefined)
   })
-  
+
 })();
 
 
 ;(function() {
   module('range')
-  
+
   var fn = Form.validators.range()
-  
+
   test('passes empty values', function() {
     equal(fn(''), undefined)
     equal(fn(null), undefined)
     equal(fn(undefined), undefined)
   })
-  
+
   test('fails non-number values', function() {
     ok(fn('foo'))
     ok(fn('123a'))
   })
-  
+
   test('accepts numbers in range', function() {
     equal(fn('12'), undefined)
     equal(fn(45), undefined)
     equal(fn(13.3), undefined)
     equal(fn('23.5'), undefined)
   })
-  
+
   test('fails numbers out of range', function() {
     ok(fn('123'))
     ok(fn(456))
     ok(fn('-1'))
     ok(fn(-2))
   })
-  
+
 })();
 
 
 ;(function() {
   module('email')
-  
+
   var fn = Form.validators.email()
-  
+
   test('passes empty values', function() {
     equal(fn(''), undefined)
     equal(fn(null), undefined)
     equal(fn(undefined), undefined)
   })
-  
+
   test('fails invalid emails', function() {
     ok(fn('invalid'))
     ok(fn('email@example'))
@@ -223,7 +223,7 @@
     ok(fn('foo@exa#mple.com'))
     ok(fn(234))
   })
-  
+
   test('accepts valid emails', function() {
     equal(fn('test@example.com'), undefined)
     equal(fn('john.smith@example.com'), undefined)
@@ -231,21 +231,21 @@
     equal(fn('john-smith@example.com'), undefined)
     equal(fn('john+smith@example.com'), undefined)
   })
-  
+
 })();
 
 
 ;(function() {
   module('url')
-  
+
   var fn = Form.validators.url()
-  
+
   test('passes empty values', function() {
     equal(fn(''), undefined)
     equal(fn(null), undefined)
     equal(fn(undefined), undefined)
   })
-  
+
   test('fails invalid url', function() {
     ok(fn('invalid'))
     ok(fn('example.com'))
@@ -254,7 +254,7 @@
     ok(fn('http://example'))
     ok(fn(234))
   })
-  
+
   test('accepts valid urls', function() {
     equal(fn('http://example.com'), undefined)
     equal(fn('http://example.co.uk'), undefined)
@@ -264,40 +264,40 @@
     equal(fn('http://www.example.com/path/1/2'), undefined)
     equal(fn('http://www.example.com/path/1/2?q=str'), undefined)
   })
-  
+
 })();
 
 
 ;(function() {
   module('match')
-  
+
   var fn = Form.validators.match({
     field: 'confirm'
   });
-  
+
   test('passes empty values', function() {
     equal(fn(''), undefined)
     equal(fn(null), undefined)
     equal(fn(undefined), undefined)
   })
-  
+
   test('accepts when fields match', function() {
     var attrs = {
       password: 'foo',
       confirm: 'foo'
     };
-    
+
     equal(fn('foo', attrs), undefined)
   })
-  
+
   test('fails when fields dont match', function() {
     var attrs = {
       password: 'foo',
       confirm: 'bar'
     };
-    
+
     var err = fn('foo', attrs)
-    
+
     equal(err.type, 'match')
     equal(err.message, 'Must match field "confirm"')
   })
